ab27d6ff3c7c58b6fb7cc6752202704b9b9c9241
AnanasekE <janeksambor@gmail.com>
2022-03-14 14:03:43 +0100

    commit

--- Small projects/minecraftMiningScript.py
+++ Small projects/minecraftMiningScript.py
@@ -3,15 +3,13 @@ import keyboard
 import pynput
 import time
 
+
 # mouse = pynput.mouse.Controller()
 # mouse.move(10, 10)
 
 
 # wh = pag.size()
 # print(wh)
-
-
-
 
 
 def square3x3():
@@ -34,6 +32,7 @@ def square3x3():
     pag.leftClick()
     pag.move(-100, 100, 0)
 
+
 def moose2():
     while True:
         try:
@@ -52,23 +51,21 @@ def moose2():
             continue
 
 
+# def moose():
+#     # mouse = Controller()
+#
+#     mouse.position = (100, 200)
+#     mouse.move(200, -100)
 
 
-def moose():
-    # mouse = Controller()
-
-    mouse.position = (100, 200)
-    mouse.move(200, -100)
-
-
-def move_smooth(xm, ym, t):
-    for i in range(t):
-        if i < t / 2:
-            h = i
-        else:
-            h = t - i
-        mouse.move(h * xm, h * ym)
-        time.sleep(1 / 60)
+# def move_smooth(xm, ym, t):
+#     for i in range(t):
+#         if i < t / 2:
+#             h = i
+#         else:
+#             h = t - i
+#         mouse.move(h * xm, h * ym)
+#         time.sleep(1 / 60)
 
 
 moose2()
