d4d5a6dd1320e59f402967be8c12371e0c755f9b
janek <janeksambor@gmail.com>
2022-03-29 15:07:02 +0200

    commit

--- TMP's/tmp1.py
+++
@@ -1,2 +0,0 @@
-for i in range(1,99999999999):
-    print(i)
--- Templates/Sorting/mergeSort.py
+++ Templates/Sorting/mergeSort.py
@@ -2,22 +2,50 @@ arr = [9, 7, 1, 2, 5, 4, 3, 2]
 list1 = []
 
 
+# def merge(arr, left, right):
+#     for i in range(0, len(left)):
+#         print('merge[i]:', i)
+#         if left[i] == right[i]:
+#             list1.append(left[i])
+#         elif left[i] > right[i] :
+#             list1.append(right[i])
+#             list1.append(left[i])
+#         else:
+#             list1.append(left[i])
+#             list1.append(right[i])
+#         print('list1',i,list1)
+
 def merge(arr, left, right):
-    for i in range(0, len(left)):
-        print('merge[i]:', i)
-        if left[i] == right[i]:
-            list1.append(left[i])
-        elif left[i] > right[i] :
-            list1.append(right[i])
-            list1.append(left[i])
+    i = 0
+    ip = 0
+    il = 0
+
+    while il < len(left) and ip < len(right):
+        if il < ip:
+            arr[i] = il
+            il += 1
         else:
-            list1.append(left[i])
-            list1.append(right[i])
-        print('list1',i,list1)
+            arr[i] = ip
+            ip += 1
+        i += 1
+    while il < len(left):
+        if il < ip:
+            arr[i] = il
+            il += 1
+        else:
+            arr[i] = ip
+            ip += 1
+        i += 1
+    while ip < len(right):
+        if ip < il:
+            arr[i] = ip
+            ip += 1
+        else:
+            arr[i] = il
+            il += 1
+        i += 1
 
 
-
-    pass
 
 
 def mergeSort(arr):
@@ -26,10 +54,11 @@ def mergeSort(arr):
         left = arr[:arrCenter]
         right = arr[arrCenter:]
         mergeSort(left)
-        print('left:',left)
+        # print('left:', left)
         mergeSort(right)
-        print('right:', right)
+        # print('right:', right)
         merge(arr, left, right)
+        return arr
 
 
-mergeSort(arr)
+print(mergeSort(arr))
